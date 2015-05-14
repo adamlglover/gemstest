f0900138805c8939c40b96488cf37e97b76ad085
Adam <adamlglover85@gmail.com>
2015-05-14 05:42:16 +0000

    last routes update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 #  get 'notifications/index'
 #get 'incoming/' => 'notifications#incoming'
-  post 'new/' => 'notifications#new'
+#  post 'new/' => 'notifications#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
